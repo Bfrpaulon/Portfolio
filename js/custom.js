6301390047b951206d6f9a0423d37a2623b40632
Bfrpaulon <brunopaulon@outlook.com.br>
2023-01-17 10:22:05 +0000

    Project Finished Uploaded

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+(function ($) {
+
+  "use strict";
+
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('.color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+    })
+
+    // HEADER
+    $(".navbar").headroom();
+
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.nav-link, .custom-btn-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });  
+
+    // TOOLTIP
+    $('.social-links a').tooltip();
+
+})(jQuery);
+
+// Send Mail
+
+function enviarEmail() {
+  var nome = document.getElementById('nome').value;
+  var email = document.getElementById('email').value;
+  var mensagem = document.getElementById('mensagem').value;
+  window.location.href = 'mailto:brunopaulon7@gmail.com?subject=' + nome + '&body=' + ' - ' + mensagem;
+}
